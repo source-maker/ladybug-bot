3ac2b2e0f08bfdb8fff01e692f8d272ccba19dcb
Daisuke Miyazaki <daisuke.miyazaki012@gmail.com>
2024-06-21 16:03:45 +0900

    feat: poc to create comments through action

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,26 @@
 "use strict";
-console.log("hello");
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@actions/core");
+const github_1 = require("@actions/github");
+async function run() {
+    var _a;
+    const name = (0, core_1.getInput)("name");
+    const token = (0, core_1.getInput)("gh-token");
+    const octokit = (0, github_1.getOctokit)(token);
+    const pullRequest = github_1.context.payload.pull_request;
+    try {
+        if (!pullRequest) {
+            throw new Error("No pull request found.");
+        }
+        await octokit.rest.issues.createComment({
+            owner: pullRequest.base.repo.owner.login,
+            repo: pullRequest.base.repo.name,
+            issue_number: pullRequest.number,
+            body: `hello ${name}`
+        });
+    }
+    catch (error) {
+        (0, core_1.setFailed)((_a = error === null || error === void 0 ? void 0 : error.message) !== null && _a !== void 0 ? _a : "Unknown error");
+    }
+}
+run();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("hello")
+import { getInput, setFailed } from "@actions/core"
+import { context, getOctokit } from "@actions/github"
+
+async function run() {
+    const name = getInput("name")
+    const token = getInput("gh-token")
+
+    const octokit = getOctokit(token)
+    const pullRequest = context.payload.pull_request
+
+    try {
+        if (!pullRequest) {
+            throw new Error("No pull request found.")
+        }
+
+        await octokit.rest.issues.createComment({
+            owner: pullRequest.base.repo.owner.login,
+            repo: pullRequest.base.repo.name,
+            issue_number: pullRequest.number,
+            body: `hello ${name}`
+        });
+    } catch (error) {
+        setFailed((error as Error)?.message ?? "Unknown error")
+    }
+}
+
+run();
